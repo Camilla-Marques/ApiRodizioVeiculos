950ec21ea3ff3bbf65e227e501dd0661deaf849d
Camilla-Marques <camilla.93marques@gmail.com>
2021-05-27 21:45:10 -0300

    Inclusão do método para consulta pelo cpf

--- api-veiculos/src/main/java/com/project/rest/api/controller/UsuarioController.java
+++ api-veiculos/src/main/java/com/project/rest/api/controller/UsuarioController.java
@@ -1,15 +1,26 @@
 package com.project.rest.api.controller;
 
+import com.project.rest.api.model.UsuarioModel;
+import com.project.rest.api.repository.UsuarioRespository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioRespository repository;
+
     @GetMapping(path = "/api/usuario/{cpf}")
-    public ResponseEntity consultar(@PathVariable("cpf")String cpf) {
-        return null;
+    public ResponseEntity consultar(@PathVariable("cpf") Long cpf) {
+        return repository.findById(cpf)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(path = "/api/usuario/salvar")
+    public UsuarioModel salvar(@RequestBody UsuarioModel usuario) {
+        return repository.save(usuario);
     }
 }
